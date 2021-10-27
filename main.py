d8d81f7de4d2e7ede91eb6874f666ac6edcc4789
ZipS1 <czwetkow.serega@yandex.ru>
2021-10-27 15:10:01 +0300

    made basic structure

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
+import pygame
+
+
+class ConsoleUI:
+    def __init__(self):
+        pass
+
+    def input_size(self):
+        pass
+
+
+class Window:
+    def __init__(self):
+        pass
+
+    def mainloop(self):
+        pass
+
+    def draw_grid(self):
+        pass
+
+    def draw_cell(self):
+        pass
+
+    def handle_click(self):
+        pass
+
+    def save_picture(self):
+        pass
+
+
+class Grid:
+    def __init__(self):
+        pass
+
+    def draw_layout(self):
+        pass
+
+    def draw_line(self):
+        pass
+
+
+class Cell:
+    def __init__(self):
+        pass
+
+    def change_state(self):
+        pass
+
+
 def main():
     pass
 
