1883deb67e6c740a8c3cf9098d10c1fdb3b4f307
ZipS1 <czwetkow.serega@yandex.ru>
2022-02-04 18:13:05 +0300

    decomposed draw method

--- window.py
+++ window.py
@@ -9,22 +9,32 @@ class Window:
     found out what object user clicked on
     """
     def __init__(self, resolution, picture_size):
-        self.resolution = resolution
-        self.picture_size = picture_size
+        self.width, self.height = resolution
+        self.cells_on_x, self.cells_on_y = picture_size
 
         self.screen = pg.display.set_mode(resolution)
         pg.display.set_caption(WINDOW_TITLE)
 
     def draw(self, cells, lines):
-        width, height = self.screen.get_size()
-        cells_in_width, cells_in_height = self.picture_size
+        self._draw_background()
+        self._draw_lines()
+
+    def _draw_background(self):
         self.screen.fill(WHITE)
 
-        for x in range(0, width + 1, round(width/cells_in_width)):
-            pg.draw.line(self.screen, BLANK_LINE_COLOR, (x, 0), (x, height), width=3)
+    def _draw_lines(self):
+        for x in range(0, self.width + 1, round(self.width/self.cells_on_x)):
+            pg.draw.line(
+                self.screen, BLANK_LINE_COLOR,
+                (x, 0), (x, self.height), width=3)
 
-        for y in range(0, height + 1, round(height/cells_in_height)):
-            pg.draw.line(self.screen, BLANK_LINE_COLOR, (0, y), (width, y), width=3)
+        for y in range(0, self.height + 1, round(self.height/self.cells_on_y)):
+            pg.draw.line(
+                self.screen, BLANK_LINE_COLOR,
+                (0, y), (self.width, y), width=3)
+
+    def _draw_cells(self):
+        pass
 
     def get_clicked_object(self):
         pass
